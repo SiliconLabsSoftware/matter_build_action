d4f764bfca89b7fe56928a2287fa5b05e694aa41
Sarthak Shaha <sarthak.shaha@silabs.com>
2025-11-14 09:28:35 -0500

    fix

--- docker/tools/build.sh
+++ docker/tools/build.sh
@@ -26,26 +26,11 @@ if [[ -z "$TAG" ]]; then
   exit 1
 fi
 
-# Example TAG: SiSDKv2024.12.1-0.de_WiFi_SDKv3.4.1
-# Extract SISDK_Tag and WiFI_SDK_Tag from TAG
-SISDK_Tag=$(echo "$TAG" | sed -n 's/^SiSDK\([^_]*\)_WiFi_SDK.*$/\1/p')
-WiFI_SDK_Tag=$(echo "$TAG" | sed -n 's/^SiSDK[^_]*_WiFi_SDK\(.*\)$/\1/p')
-
-if [[ -z "$SISDK_Tag" || -z "$WiFI_SDK_Tag" ]]; then
-  echo "ERROR: Could not parse SISDK_Tag or WiFI_SDK_Tag from version tag: $TAG"
-  exit 1
-fi
-
-echo "Parsed SISDK_Tag: $SISDK_Tag"
-echo "Parsed WiFI_SDK_Tag: $WiFI_SDK_Tag"
-
 # Compose image tag
 IMAGE_NAME="ghcr.io/siliconlabssoftware/matter_extension_dependencies"
 
 # Build the Docker image, passing build args
 docker build \
-  --build-arg SISDK_Tag="$SISDK_Tag" \
-  --build-arg WiFI_SDK_Tag="$WiFI_SDK_Tag" \
   -f docker/Dockerfile \
   -t "${IMAGE_NAME}:${TAG}" .
 
